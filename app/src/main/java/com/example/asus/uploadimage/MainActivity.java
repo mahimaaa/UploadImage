b832224fd1588d3642cda91d9696b4215cc3b9d0
Mahima <mandhanams@rknec.edu>
2018-07-05 16:22:57 +0530

    hi

---
+++ app/src/main/java/com/example/asus/uploadimage/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.asus.uploadimage;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    Button butt;
+    ImageView i;
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT=2;
+    private Uri downloadUrl;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.upload);
+       // butt=(Button)findViewById(R.id.button1);
+        //i=(ImageView)findViewById(R.id.im);
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setType("image/*");
+                startActivityForResult(i, GALLERY_INTENT);
+
+
+            }
+        });;
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_INTENT&&resultCode==RESULT_OK)
+        {
+            Uri file = data.getData();
+            StorageReference riversRef = mStorage.child("Photo").child("bastar").child("cuisine1").child("!");
+
+            riversRef.putFile(file)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            //  Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                            Toast.makeText(MainActivity.this,"Daalra",Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            // ...
+                            Toast.makeText(MainActivity.this,"nai Daalra",Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+    }
+
+}
